93d80729f946739234b6073489be875297f07ece
Bryan Lopez <bryanantoniolopez@udetijuana.edu.mx>
2025-12-02 09:45:10 -0800

    Refactor code structure for improved readability and maintainability

--- data/projects.ts
+++ data/projects.ts
@@ -1,13 +1,13 @@
-export type ProjectType = "gallery" | "single" | "pdf" | "complete"
+export type ProjectType = "gallery" | "single" | "pdf" | "complete";
 
 export interface Project {
-  category: string
-  title: string
-  description: string
-  type: ProjectType
-  thumbnail: string
-  images?: string[]
-  pdfUrl?: string
+  category: string;
+  title: string;
+  description: string;
+  type: ProjectType;
+  thumbnail: string;
+  images?: string[];
+  pdfUrl?: string;
 }
 
 export const projects: Project[] = [
@@ -20,24 +20,26 @@ export const projects: Project[] = [
     thumbnail: "projects/brilliant-dental-laboratory/1.jpeg",
     images: [
       "projects/brilliant-dental-laboratory/1.jpeg",
-        "projects/brilliant-dental-laboratory/2.jpeg",
+      "projects/brilliant-dental-laboratory/2.jpeg",
     ],
   },
   {
     category: "fotografia",
     title: "Sesión Fotográfica - XV Años",
-    description: "Sesión completa de fotografía de xv años, incluyendo edición profesional.",
+    description:
+      "Sesión completa de fotografía de xv años, incluyendo edición profesional.",
     type: "gallery",
     thumbnail: "projects/sesion-1/1.jpg",
-    images: ["projects/sesion-1/1.jpg", "projects/sesion-1/2.jpg"]
+    images: ["projects/sesion-1/1.jpg", "projects/sesion-1/2.jpg"],
   },
-    {
+  {
     category: "fotografia",
     title: "Sesión Fotográfica - XV Años",
-    description: "Sesión completa de fotografía de xv años, incluyendo edición profesional.",
+    description:
+      "Sesión completa de fotografía de xv años, incluyendo edición profesional.",
     type: "gallery",
     thumbnail: "projects/sesion-2/1.jpg",
-    images: ["projects/sesion-2/1.jpg"]
+    images: ["projects/sesion-2/1.jpg"],
   },
   {
     category: "ilustracion",
@@ -50,10 +52,12 @@ export const projects: Project[] = [
   {
     category: "ilustracion",
     title: "New Life Hospital",
-    description: "Manual de identidad corporativa para New Life Hospital. Hospital especializado en servicios bariátricos",
+    description:
+      "Manual de identidad corporativa para New Life Hospital. Hospital especializado en servicios bariátricos",
     type: "pdf",
     thumbnail: "projects/new-life-hospital/1.jpeg",
-    pdfUrl: "/projects/new-life-hospital/Manual de identidad New Life Hospital.pdf",
+    pdfUrl:
+      "/projects/new-life-hospital/Manual de identidad New Life Hospital.pdf",
   },
   {
     category: "ilustracion",
@@ -70,5 +74,80 @@ export const projects: Project[] = [
       "projects/nakeji-dental/6.jpeg",
       "projects/nakeji-dental/7.jpeg",
     ],
-  }
+  },
+    {
+    category: "impresos",
+    title: "Grupo Concordia",
+    description:
+      "Diseño e impresión para corporativo para empresa de construcción.",
+    type: "gallery",
+    thumbnail: "projects/grupo-concordia/1.jpg",
+    images: [
+      "projects/grupo-concordia/1.jpg",
+      "projects/grupo-concordia/2.jpg",
+    ],
+  },
+  {
+    category: "impresos",
+    title: "Otay Campestre",
+    description:
+      "Diseño e impresión de material promocional para club deportivo.",
+    type: "gallery",
+    thumbnail: "projects/otay-campestre/2.jpg",
+    images: ["projects/otay-campestre/1.jpg", "projects/otay-campestre/2.jpg"],
+  },
+    {
+    category: "impresos",
+    title: "Geoingenieros",
+    description:
+      "Diseño e impresión de papelería corporativa para empresa de construcción.",
+    type: "single",
+    thumbnail: "projects/geoingenieros/1.jpg",
+    images: ["projects/geoingenieros/1.jpg"],
+  },
+    {
+    category: "impresos",
+    title: "The Bakery",
+    description:
+      "Diseño e impresión material promocional para panadería local.",
+    type: "gallery",
+    thumbnail: "projects/the-bakery/3.jpg",
+    images: [
+      "projects/the-bakery/1.jpg",
+      "projects/the-bakery/2.jpg",
+      "projects/the-bakery/3.jpg",
+    ],
+  },
+  {
+    category: "impresos",
+    title: "Personaje de Pizzeria",
+    description:
+      "Diseño e impresión de personaje ilustrado para pizzería local.",
+    type: "gallery",
+    thumbnail: "projects/pizzeria/2.jpg",
+    images: ["projects/pizzeria/1.jpg", "projects/pizzeria/2.jpg"],
+  },
+
+  {
+    category: "impresos",
+    title: "Maccato",
+    description: "Diseño e impresión de material promocional para cafetería.",
+    type: "gallery",
+    thumbnail: "projects/maccato/1.jpg",
+    images: [
+      "projects/maccato/1.jpg",
+      "projects/maccato/2.jpg",
+      "projects/maccato/3.jpg",
+    ],
+  },
+  {
+    category: "impresos",
+    title: "Ace Dentistry",
+    description:
+      "Diseño gráfico e impresión de material promocional para clínica dental.",
+    type: "gallery",
+    thumbnail: "projects/ace-dentistry/2.jpg",
+    images: ["projects/ace-dentistry/1.jpg", "projects/ace-dentistry/2.jpg"],
+  },
+
 ];
